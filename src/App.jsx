695034f2ba98c4e3fed470d1db217d8b4164d7c9
milliorn <scottmilliorn@gmail.com>
2022-10-01 16:09:28 -0700

    Coins

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-const URL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+import Coins from "./components/Coins";
 
 function App() {
+  const URL =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
   const [coins, setCoins] = useState([]);
 
   /* get data from api and store it in setCoins */
@@ -20,7 +21,7 @@ function App() {
       });
   }, []);
 
-  return <>Hello World</>;
+  return <Coins coins={coins} />;
 }
 
 export default App;
--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Coins = () => {
+const Coins = (props) => {
   return (
-    <div>Coins</div>
-  )
-}
+    <div className="container">
+      <div className="heading">
+        <p>#</p>
+        <p className="coin-name">Coin</p>
+        <p>Price</p>
+        <p>24hr</p>
+        <p className="hide-mobile">Volume</p>
+        <p className="hide-mobile">Market Cap</p>
+      </div>
+    </div>
+  );
+};
 
-export default Coins
+export default Coins;
