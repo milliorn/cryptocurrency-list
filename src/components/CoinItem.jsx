f18378a01b534cff3b22376cf7c570e6a9c3115a
milliorn <scottmilliorn@gmail.com>
2022-10-01 16:19:14 -0700

    CoinItem

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const CoinItem = () => {
+const CoinItem = (props) => {
   return (
-    <div>CoinItem</div>
-  )
-}
+    <div className="coin-row">
+      <p>{props.coins.market_cap_rank}</p>
+      <div className="img-symbol">
+        <img src={props.coins.image} alt="" srcset="" />
+        <p>{props.coins.symbol}</p>
+      </div>
+      <p>{props.coins.current_price}</p>
+      <p>{props.coins.price_change_percentage_24h}</p>
+      <p className="hide-mobile">{props.coins.total_volume}</p>
+      <p className="hide-mobile">{props.coins.market_cap}</p>
+    </div>
+  );
+};
 
-export default CoinItem
+export default CoinItem;
