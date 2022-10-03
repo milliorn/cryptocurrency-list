833cfd9aca41a232d2ecc8318738ec396a43f856
milliorn <scottmilliorn@gmail.com>
2022-10-02 19:19:23 -0700

    CoinItem

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import "../styles/Coins.css";
-
 const styles = {
   coinRow:
     "flex cursor-pointer	justify-between	items-center bg-zinc-800 shadow-lg shadow-zinc-900 rounded-lg	my-8 mx-4 hover:scale-105	ease-in-out duration-300",
+  hidden: "hidden md:contents",
+  img: "h-10	mr-2 w-auto	",
   imgSymbol: "flex items-center",
 };
 
@@ -13,13 +13,15 @@ const CoinItem = (props) => {
     <div className={styles.coinRow}>
       <p>{props.coins.market_cap_rank}</p>
       <div className={styles.imgSymbol}>
-        <img src={props.coins.image} alt="" />
+        <img src={props.coins.image} alt="" className={styles.img} />
         <p>{props.coins.symbol.toUpperCase()}</p>
       </div>
       <p>{props.coins.current_price.toLocaleString()}</p>
       <p>{props.coins.price_change_percentage_24h.toFixed(2)}%</p>
-      <p className="hide-mobile">{props.coins.total_volume.toLocaleString()}</p>
-      <p className="hide-mobile">{props.coins.market_cap.toLocaleString()}</p>
+      <p className={styles.hidden}>
+        {props.coins.total_volume.toLocaleString()}
+      </p>
+      <p className={styles.hidden}>{props.coins.market_cap.toLocaleString()}</p>
     </div>
   );
 };
